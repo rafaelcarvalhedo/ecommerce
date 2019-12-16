d4bb78ed34dff2bea1ab815eeec1314e873dd4a8
rafael <rafaelcsousa21@gmail.com>
2019-12-16 17:33:09 -0300

    erro consulta produto

--- src/main/java/br/com/mecommerce/entity/User.java
+++ src/main/java/br/com/mecommerce/entity/User.java
@@ -8,7 +8,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "order_id")
+    @Column(name = "user_id")
     private Integer id;
     private String login;
     private String password;
